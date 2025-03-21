47a57923314bf24c88738aa687ba2b257c743b08
Tanisha <vermatanisha620@gmail.com>
2025-03-22 00:58:43 +0530

    stack navigation with expo router

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,13 @@
 import { ShoppingListItem } from "@/components/ShoppiningListItem";
+import { Link } from "expo-router";
 import { StyleSheet, View} from "react-native";
 
 export default function Index() {
   return (
     <View style={styles.container}>
+      <Link href="/counter" style={{ textAlign: "center", marginBottom: 18, fontSize: 24 }}>
+        Go to counter
+      </Link>
        <ShoppingListItem name="coffee" />
        <ShoppingListItem name="Tea" isCompleted />
        <ShoppingListItem name="Sugar" isCompleted />
