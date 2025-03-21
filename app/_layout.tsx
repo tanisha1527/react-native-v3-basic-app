8dd55b6c818ffd9e734dac8b6404549591915550
Tanisha <vermatanisha620@gmail.com>
2025-03-22 01:23:31 +0530

    Set up bottom tabs navigation

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,28 @@
-import { Stack } from "expo-router";
+import { Tabs } from "expo-router";
+import Feather from '@expo/vector-icons/Feather';
+import FontAwesome5 from '@expo/vector-icons/FontAwesome5';
+import { theme } from "@/theme";
+
 
 export default function RootLayout() {
-  return <Stack screenOptions={{title: "Shopping List"}}/>;
+  return (
+     <Tabs screenOptions={{ tabBarActiveTintColor: theme.colorCerulean}}>
+        <Tabs.Screen name="index" options={{title: 
+        "Shopping List", 
+        tabBarIcon: ({ color, size }) =>{
+          return <Feather name="list" size={size} color={color} />
+        }
+        }}/>
+        <Tabs.Screen name="counter" options={{title: "counter",
+          tabBarIcon: ({ color, size }) =>{
+            return <Feather name="clock" size={size} color={color} />
+          }
+        }}/>
+        <Tabs.Screen name="idea" options={{title: "idea",
+          tabBarIcon: ({ color, size }) =>{
+            return <FontAwesome5 name="lightbulb" size={size} color={color} />
+          }
+        }}/>
+     </Tabs>
+  );
 }
