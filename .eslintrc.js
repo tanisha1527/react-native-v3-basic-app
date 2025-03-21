708ba12e56fa88db1ff28c9e9d92b563b71d13d3
Tanisha <vermatanisha620@gmail.com>
2025-03-21 14:38:45 +0530

    Set up Environment, eslint and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+// https://docs.expo.dev/guides/using-eslint/
+mmodule.exports = {
+  extends: ["expo", "prettier"],
+  plugins: ["prettier"],
+  rules: {
+    "prettier/prettier": "error",
+  },
+};
