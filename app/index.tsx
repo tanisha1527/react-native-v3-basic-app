4402c48df74f0aefc36d3fe757c169681165dd74
Tanisha <vermatanisha620@gmail.com>
2025-03-21 21:37:01 +0530

    Add a delete button

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,31 @@
-import { StyleSheet, Text, View } from "react-native";
+import { theme } from "@/theme";
+import { StyleSheet, Text, View, Alert, TouchableOpacity } from "react-native";
 
 export default function Index() {
+  const handleDelete = () => {
+    Alert.alert("Delete Item", "Are you sure you want to delete this item?", [
+      {
+        text: "Cancel",
+        onPress: () => console.log("Cancel Pressed"),
+        style: "cancel",
+      },
+      {
+        text: "YES",
+        onPress: () => console.log("OK Pressed"),
+      },
+    ]);
+  }
+
   return (
     <View style={styles.container}>
       <View style={styles.itemContainer}>
         <Text style={styles.itemText}>Coffee</Text>
+        <TouchableOpacity
+         style={styles.button} 
+         onPress={handleDelete} 
+         activeOpacity={0.8}>
+         <Text style={styles.buttonText}>Delete</Text>
+      </TouchableOpacity>
       </View>
     </View>
   );
@@ -15,7 +36,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-
     justifyContent: "center",
   },
   itemContainer: {
@@ -23,9 +43,23 @@ const styles = StyleSheet.create({
     borderBottomColor: "#1a759f", 
     paddingHorizontal: 8, 
     paddingVertical: 16,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
   },
   itemText: {
     fontSize: 18,
     fontWeight: "200",
+  },
+  button: {
+    backgroundColor: theme.colorBlack,
+    padding: 8,
+    borderRadius: 6,
+  },
+  buttonText: {
+    color: theme.colorWhite,
+    fontWeight: "bold",
+    textTransform: "uppercase",
+    letterSpacing: 1,
   },
 })
