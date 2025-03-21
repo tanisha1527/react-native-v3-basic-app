a23f92b0158b32e7b51f3bd4e7bfe273ee1b89e5
Tanisha <vermatanisha620@gmail.com>
2025-03-21 21:16:19 +0530

    Display shopping list item name

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,31 @@
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.container}>
+      <View style={styles.itemContainer}>
+        <Text style={styles.itemText}>Coffee</Text>
+      </View>
     </View>
   );
 }
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+
+    justifyContent: "center",
+  },
+  itemContainer: {
+    borderBottomWidth: 1, 
+    borderBottomColor: "#1a759f", 
+    paddingHorizontal: 8, 
+    paddingVertical: 16,
+  },
+  itemText: {
+    fontSize: 18,
+    fontWeight: "200",
+  },
+})
