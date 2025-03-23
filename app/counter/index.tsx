48dfaf24d6e48c629a4fbb4ccd703c2fb0a75263
Tanisha <vermatanisha620@gmail.com>
2025-03-23 14:41:14 +0530

    Add local push notifications

--- app/counter/index.tsx
+++ app/counter/index.tsx
@@ -1,9 +1,39 @@
-import { Text, View, StyleSheet } from "react-native";
+import { theme } from "@/theme";
+import { registerForPushNotificationsAsync } from "@/Utils/registerForPushNotificationsAsync";
+import { Text, View, StyleSheet, TouchableOpacity, Alert } from "react-native";
+import * as Device from "expo-device";
+import * as Notifications from "expo-notifications";
 
 export default function CounterScreen() {
 
+    const scheduleNotification = async () => {
+        const result = await registerForPushNotificationsAsync();
+        if (result === "granted") {
+            await Notifications.scheduleNotificationAsync({
+                 content: {
+                    title: "I'm a notification from your app! 📨",
+                 },
+                 trigger: {                                                                                                    
+                    seconds: 2,
+                 },
+            });
+        } else {
+            if (Device.isDevice) {
+                 Alert.alert(
+                       "Unable to schedule notification",
+                       "Enable the notifications permission for Expo Go in settings",
+                     );
+            }
+    };
+
+}
+
     return (
         <View style={styles.container}>
+            <TouchableOpacity style={styles.button} activeOpacity={0.8}
+                onPress={scheduleNotification}>
+                <Text style={styles.text}>Schedule Notification</Text>
+            </TouchableOpacity>
             <Text style={styles.text}>Counter</Text>
         </View>
     );
@@ -17,6 +47,14 @@ const styles = StyleSheet.create({
         backgroundColor: "#fff",
     },
     text: {
-        fontSize: 24,
+        color: theme.colorWhite,
+        fontWeight: "bold",
+        textTransform: "uppercase",
+        letterSpacing: 1,
+    },
+    button: {
+        backgroundColor: theme.colorBlack,
+        padding: 12,
+        borderRadius: 6,
     },
 });
